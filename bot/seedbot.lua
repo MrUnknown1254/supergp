1e85cf8f5971967a1c5d18de6ada823aedb341de
MrUnknown1254 <MrUnknown1254@gmail.com>
2016-07-02 13:48:19 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,31 +226,18 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {78916979,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
-]],
+    about_text = [[Zs TG V1.5
+    >Zs TG Comming Back :)
+    
+    Sudo 
+    > @Zs_Sudo
+    
+    Our channel 
+    > @Zs_TG_CH
+    GoodLuck :) ]],
     help_text_realm = [[
 Realm Commands:
 
